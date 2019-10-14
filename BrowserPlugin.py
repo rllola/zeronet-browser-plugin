07b452f05688fd6fc9c5a80ba1b0184ff88741b7
rllola <rllola80@gmail.com>
2019-10-14 23:06:55 +0200

    Update message for update without the required permission

--- BrowserPlugin.py
+++ BrowserPlugin.py
@@ -1,10 +1,11 @@
 import sys
 import os
+import subprocess
 
 from Plugin import PluginManager
 from Translate import translate as _
 from util.Flag import flag
-
+from Config import config
 
 @PluginManager.registerTo("UiWebsocket")
 class UiWebsocketPlugin(object):
@@ -29,8 +30,15 @@ class UiWebsocketPlugin(object):
             #main.file_server.stop()
             #main.ui_server.stop()
 
-        self.cmd(
-            "confirm",
-            [_["Update <b>ZeroNet client</b> to latest version?"], _["Update"]],
-            cbServerUpdate
-        )
+        if os.access(os.getcwd(), os.W_OK):
+            self.cmd(
+                "confirm",
+                [_["Update <b>ZeroNet client</b> to latest version?"], _["Update"]],
+                cbServerUpdate
+            )
+
+        else:
+            self.cmd(
+                "notification",
+                ["info", _["You don't seem to have the required permissions to do the update <br/> You fix this by adding the current user to the zeronet-browser group <br/> <b>sudo usermod -a -G zeronet-browser $USER</b> "]]
+            )
